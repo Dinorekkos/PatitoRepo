0f9cfef353ea5eaf6b653cffaf9d242eb6540028
Will <will_bates@live.com.mx>
2021-01-07 02:06:08 -0600

    Implementacion de enemigos y vida del personaje

---
+++ Assets/Scripts/Game/Gameplays/Mosquitoes/EnemyController_Mosquitoes.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController_Mosquitoes : MonoBehaviour
+{
+    #region public methods
+    public void Initialize()
+    {
+        float targetSpeed = Random.Range(minMaxSpeed.x, minMaxSpeed.y);
+
+        Speed = targetSpeed;
+    }
+    #endregion
+
+    #region public variables
+    public float Speed
+    {
+        get { return _speed; }
+        private set { _speed = value; }
+    }
+    #endregion
+
+    #region private methods
+    private void Update()
+    {
+        //Updates the target position
+        Vector3 targetPosition = transform.position;
+        targetPosition.x -= Speed * Time.deltaTime;
+        transform.position = targetPosition;
+
+        if (transform.position.x <= minPosition)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    #endregion
+
+    #region private variables
+    [SerializeField]
+    private Vector2 minMaxSpeed = new Vector2(4,6);
+
+    [SerializeField]
+    private float minPosition = -10f;
+
+    private float _speed = 0;
+    #endregion
+}
---
+++ Assets/Scripts/Game/Gameplays/Mosquitoes/EnemySpawnpoint_Mosquitoes.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gameplays.Mosquitoes
+{
+    public class EnemySpawnpoint_Mosquitoes : MonoBehaviour
+    {
+        #region public methods
+        //Disable spawnpoint for a time
+        public void UseSpawnPoint()
+        {
+            IsActive = false;
+            OnStateChange?.Invoke(this);
+            StartCoroutine(Cooldown());
+        }
+
+        public Vector3 GetSpawnPosition()
+        {
+            return this.transform.position;
+        }
+        #endregion
+
+        #region public variables
+        public bool IsActive
+        {
+            get { return _isActive; }
+            private set { _isActive = value; }
+        }
+
+        public System.Action<EnemySpawnpoint_Mosquitoes> OnStateChange;
+        #endregion
+
+        #region private methods
+        private IEnumerator Cooldown()
+        {
+            //Wait for cooldown time
+            yield return new WaitForSeconds(cooldownTime);
+            IsActive = true;
+            OnStateChange?.Invoke(this);
+        }
+        #endregion
+
+        #region private variables
+        [SerializeField]
+        private float cooldownTime = 2f;
+
+        private bool _isActive = true;
+        #endregion
+    }
+}
