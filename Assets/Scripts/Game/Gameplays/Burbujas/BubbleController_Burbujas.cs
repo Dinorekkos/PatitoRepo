55e95715001f38d6177c3038f65a5b01de15fa92
Will <will_bates@live.com.mx>
2021-03-07 16:13:53 -0600

    Cambios para que las burbujas exploten si les pegas desde abajo

--- Assets/Scripts/Game/Gameplays/Burbujas/BubbleController_Burbujas.cs
+++ Assets/Scripts/Game/Gameplays/Burbujas/BubbleController_Burbujas.cs
@@ -19,6 +19,13 @@ namespace Gameplays.Burbujas
             myCollider.enabled = false;
         }
 
+        public void InstaExplode()
+        {
+            myCollider.enabled = false;
+
+            myAnimator.SetTrigger(TRIGGER_INSTA_EXPLODE_NAME);
+        }
+
         public void FinishExplodeAnimationEvent()
         {
             hasExploded = true;
@@ -116,6 +123,7 @@ namespace Gameplays.Burbujas
         private Collider2D myCollider;
 
         private const string TRIGGER_EXPLODE_NAME = "explode";
+        private const string TRIGGER_INSTA_EXPLODE_NAME = "instaExplode";
         #endregion
     }
 }
