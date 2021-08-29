2b2078127f949f436a17dc1d069fa555965b973d
WilhemB <will_bates@live.com.mx>
2021-08-29 17:48:41 -0500

    Se importaron scripts para la resolucion de la camara, y se creo una escena CameraScaleExample donde se utilizan los scripts

---
+++ Assets/Scripts/Utility/Camera/CanvasScalerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Misc
+{
+    [ExecuteInEditMode]
+    public class CanvasScalerController : MonoBehaviour
+    {
+        public DeviceOrientation deviceOrientation = DeviceOrientation.Landscape;
+        public ScaleType type = ScaleType.AspectRatioDepending;
+
+        public float currentAspect;
+        public CanvasScaler canvasScaler;
+
+        private float lastCanvasScale = -1000;
+        private float currentCanvasScale;
+        void Update()
+        {
+            //Aspect reference depends on device orientation
+            if (deviceOrientation == DeviceOrientation.Portrait)
+            {
+                currentAspect = ((float)Screen.width / (float)Screen.height);
+            }
+            else if (deviceOrientation == DeviceOrientation.Landscape)
+            {
+                currentAspect = ((float)Screen.height / (float)Screen.width);
+            }
+
+            switch (type)
+            {
+                case ScaleType.Width:
+                    ScaleWidth();
+                    break;
+                case ScaleType.Height:
+                    ScaleHeight();
+                    break;
+                case ScaleType.Middle:
+                    ScaleMiddle();
+                    break;
+                case ScaleType.AspectRatioDepending:
+                    if (currentAspect > 0.5f)
+                    {
+                        ScaleHeight();
+                    }
+                    else
+                    {
+                        ScaleWidth();
+                    }
+                    break;
+            }
+        }
+        void ScaleWidth()
+        {
+            currentCanvasScale = 0f;
+            UpdateCanvasScale();
+        }
+        void ScaleHeight()
+        {
+            currentCanvasScale = 1f;
+            UpdateCanvasScale();
+        }
+        void ScaleMiddle()
+        {
+            currentCanvasScale = 0.5f;
+            UpdateCanvasScale();
+        }
+
+        void UpdateCanvasScale()
+        {
+            if (currentCanvasScale != lastCanvasScale)
+            {
+                if (canvasScaler != null)
+                    canvasScaler.matchWidthOrHeight = currentCanvasScale;
+            }
+            lastCanvasScale = currentCanvasScale;
+        }
+    }
+}
---
+++ Assets/Scripts/Utility/Camera/MainCameraScaler.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class MainCameraScaler : MonoBehaviour
+{
+    public bool IsEnabled = false;
+    public Camera myCamera;
+    public int targetWidth = 750;
+    public int targetHeight = 1334;
+    [SerializeField]
+    private float orthographicSize = 5;
+    public ScaleType type;
+
+    public float normalAspect;
+    public float currentAspect;
+
+    private float currentOrthographicSize;
+
+    public bool viewOnUpdate = true;
+
+    public float OrthographicSize
+    {
+        get
+        {
+            return orthographicSize;
+        }
+        set
+        {
+            orthographicSize = value;
+            UpdateScale();
+        }
+    }
+
+    void UpdateScale()
+    {
+        normalAspect = ((float)targetWidth / (float)targetHeight);
+        currentAspect = ((float)Screen.width / (float)Screen.height);
+
+        if (!IsEnabled)
+        {
+            return;
+        }
+
+        switch (type)
+        {
+            case ScaleType.Width:
+                ScaleWidth();
+                break;
+            case ScaleType.Height:
+                ScaleHeight();
+                break;
+            case ScaleType.Middle:
+                ScaleMiddle();
+                break;
+            case ScaleType.AspectRatioDepending:
+                if (currentAspect >= 0.5)
+                {
+                    ScaleHeight();
+                }
+                else
+                {
+                    ScaleWidth();
+                }
+                break;
+        }
+    }
+
+    void ScaleWidth()
+    {
+        currentOrthographicSize = OrthographicSize * normalAspect / currentAspect;
+        UpdateCameraSize();
+    }
+    void ScaleHeight()
+    {
+        currentOrthographicSize = OrthographicSize;
+        UpdateCameraSize();
+    }
+    void ScaleMiddle()
+    {
+        currentOrthographicSize = (OrthographicSize + (OrthographicSize * normalAspect / currentAspect)) / 2;
+        UpdateCameraSize();
+    }
+    void UpdateCameraSize()
+    {
+        Debug.Assert(myCamera != null, "My Camera is not asigned for gameobject: " + gameObject.name);
+        myCamera.orthographicSize = currentOrthographicSize;
+    }
+
+    public static MainCameraScaler Instance;
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        UpdateScale();
+    }
+
+#if UNITY_EDITOR
+    void Update()
+    {
+        if (viewOnUpdate)
+        {
+            UpdateScale();
+        }
+    }
+#endif
+}
